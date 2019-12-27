cc8019ac83d94d8486b9750367d55d69aca2fc3d
Laura Collins <laura.e.collins1@gmail.com>
2019-12-27 10:15:48 -0600

    data done

---
+++ src/helpers/data/employeesData.js
@@ -0,0 +1,22 @@
+const employees = [
+  {
+    id: 'employee1',
+    firstName: 'Jack',
+    lastName: 'Sparrow',
+    phoneNumber: '555-695-8593',
+  },
+  {
+    id: 'employee2',
+    firstName: 'Bat',
+    lastName: 'Man',
+    phoneNumber: '1-800-kick-ass',
+  },
+  {
+    id: 'employee3',
+    firstName: 'Jaina',
+    lastName: 'Proudmore',
+    phoneNumber: '555-111-3333',
+  },
+];
+
+export default { employees };
