cc204fdfc45dc02ef54f9496be16c60173e0ec60
Laura Collins <laura.e.collins1@gmail.com>
2019-12-29 10:11:42 -0600

    firebase db setup and reading

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,9 +12,24 @@ class Home extends React.Component {
   }
 
   componentDidMount() {
-    const dogs = dogsData.getAllDogs();
-    const employees = employeesData.getAllEmployees();
-    this.setState({ dogs, employees });
+    this.getDogs();
+    this.getEmployees();
+  }
+
+  getEmployees = () => {
+    employeesData.getAllEmployees()
+      .then((employees) => {
+        this.setState({ employees });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  getDogs = () => {
+    dogsData.getAllDogs()
+      .then((dogs) => {
+        this.setState({ dogs });
+      })
+      .catch((error) => console.error(error));
   }
 
   render() {
