4890e5419a86d696905c7a25c6df92d4e83c9a28
Laura Collins <laura.e.collins1@gmail.com>
2019-12-29 11:12:49 -0600

    walks are reading

--- src/components/SingleWalk/SingleWalk.js
+++ src/components/SingleWalk/SingleWalk.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import walkShape from '../../helpers/propz/walkShape';
+import dogsData from '../../helpers/data/dogsData';
+import employeeData from '../../helpers/data/employeesData';
 
 class SingleWalk extends React.Component {
   static propTypes = {
     walk: walkShape.walkShape,
   }
 
+  state = {
+    dogName: '',
+    employeeName: '',
+  }
+
+  componentDidMount() {
+    this.getDog();
+    this.getEmployee();
+  }
+
+  getDog = () => {
+    const { walk } = this.props;
+    dogsData.getDogById(walk.dogId)
+      .then((dog) => {
+        const dogName = dog.data.name;
+        this.setState({ dogName });
+      })
+      .catch((error) => console.error(error));
+  }
+
+  getEmployee = () => {
+    const { walk } = this.props;
+    employeeData.getEmployeeById(walk.employeeId)
+      .then((employee) => {
+        const employeeFirstName = employee.data.firstName;
+        const employeeLastName = employee.data.lastName;
+        const employeeName = `${employeeFirstName} ${employeeLastName}`;
+        this.setState({ employeeName });
+      })
+      .catch((error) => console.error(error));
+  }
+
   render() {
     const { walk } = this.props;
+
     return (
       <div className="card col-3 m-3">
       <div className="card-body">
-        <h5 className="card-title">{walk.dogId}</h5>
-        <p className="card-text">{walk.employeeId}</p>
+        <h5 className="card-title">{this.state.dogName}</h5>
+        <p className="card-text">{this.state.employeeName}</p>
         <p className="card-text">{walk.date}</p>
       </div>
     </div>
